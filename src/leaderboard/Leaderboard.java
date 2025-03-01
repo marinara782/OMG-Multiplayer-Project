363af8468dc998f73bbad7af7807636e0963dc5e
30229680 <dilraj.deol@ucalgary.ca>
2025-03-01 14:04:23 -0700

    Implemented Skeleton Code for Leaderboard class. Subject to change and still needs implementation of all methods logic

--- src/leaderboard/Leaderboard.java
+++ src/leaderboard/Leaderboard.java
@@ -1,4 +1,52 @@
 package leaderboard;
 
+import java.util.List;
+
+/**
+ * The Leaderboard class manages player rankings, update the scores of players,
+ * and provides functionality to export and import leaderboard data
+ */
 public class Leaderboard {
+    /**
+     * Add a player to the leaderboard
+     * @param playerName name of the player to be added
+     * @param playerScore the score of the player (potentially change to be a win-loss-tie format
+     *                    instead of a single score: to be determined later)
+     */
+    public void addPlayer(String playerName, int playerScore) {
+        // TODO: implement logic to add a player to the leaderboard
+    }
+
+    /**
+     * Update the score of a player.
+     * Note: potentially change this or add a new method that adds an integer value to a players current score
+     * @param playerName name of player that's score is to be updated
+     * @param newScore the new score of the player
+     */
+    public void updateScore(String playerName, int newScore) {
+        // TODO: implement logic to update the score of a player
+    }
+
+    /**
+     * Retrieves the top players based on their score
+     * @param count the number of top players to retrieve
+     * @return a list of top players
+     */
+    public List<String> getTopPlayers(int count) {
+        // TODO: implement logic to retrieve the top 'count' number of players
+        return null;
+    }
+
+    public void exportLeaderboard(String filePath) {
+        // TODO: implement the logic to export leaderboard data to a file
+        // most most likely will call a file writer class, to be implemented later
+    }
+
+    /**
+     * Displays the leaderboard in a formatted manner
+     */
+    public void displayLeaderboard() {
+        // TODO: implement logic to display the leaderboard
+        // note: this method may not be needed, as exportLeaderboard may be sufficient
+    }
 }

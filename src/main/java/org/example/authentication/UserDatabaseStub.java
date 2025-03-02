48999164e884a84e3380e566960c122ddcd3085c
30204632 <jayden.wong1@ucalgary.ca>
2025-03-02 12:31:38 -0700

    Added pseudo-code for UserDatabaseStub.java

--- src/main/java/org/example/authentication/UserDatabaseStub.java
+++ src/main/java/org/example/authentication/UserDatabaseStub.java
@@ -1,4 +1,31 @@
 package org.example.authentication;
 
+import java.util.Map;
+
 public class UserDatabaseStub {
+    /*
+
+    public String[] getUserIds(){
+        // String [] temp = some code to request from external server
+        // return temp;
+    }
+
+    // First is userID
+    // Second is password
+    public Map<String, String> getProfiles(){
+        // Map<String, String> temp = some code to contact external server
+        // returns temp;
+    }
+
+    public String getPassword(String user){
+        Map<String, String> temp = getProfiles();
+        try{
+            return temp.get(user);
+        } catch (Exception e){
+        }
+    }
+
+     */
+
+
 }

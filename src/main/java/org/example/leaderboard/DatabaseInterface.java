8fc9ba3543ea243a18d24e6d2b714147edf7a871
Ual Alcano <jerahmeelual.alcano@ucalgary.ca>
2025-03-21 14:36:40 -0600

    fixed stub

---
+++ src/main/java/org/example/leaderboard/DatabaseInterface.java
@@ -0,0 +1,14 @@
+package org.example.leaderboard;
+
+import java.util.List;
+
+public interface  DatabaseInterface {
+
+    /**
+     * obtains the list of rankings in a specific game
+     * 
+     * @param gameName name of game like Checkers, tictactoe
+     * @return list of ranking entries each containing player name, wins and losses
+     */
+    List<RankingEntry> getRankings(String gameName);
+}

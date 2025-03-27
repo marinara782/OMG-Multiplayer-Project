ac48e0209a264b32812a16c314c6e544ba1e6ab0
30232092 <jacob.baggott@ucalgary.ca>
2025-03-27 13:52:10 -0600

    commit 1. Adding methods to checkers game. All stubs.

--- src/main/java/org/example/game/checkers/CheckersGame.java
+++ src/main/java/org/example/game/checkers/CheckersGame.java
@@ -1,4 +1,18 @@
 package org.example.game.checkers;
 
 public class CheckersGame {
+
+        public void makeMove(int row, int col) {
+            // TODO: Implement logic to move
+        }
+
+        public boolean checkForWin() {
+            // TODO: Implement logic to check for a win
+            return false;
+        }
+
+        public boolean isBoardFull() {
+            // TODO: Implement logic to check if board is full
+            return false;
+        }
 }

fcb250c7fb491005ef58a988958b95e31a9be702
30153076 <jerahmeelual.alcano@ucalgary.ca>
2025-03-27 11:44:50 -0600

    completed initalizePlayers method

--- src/main/java/org/example/leaderboard/LeaderboardDatabaseStub.java
+++ src/main/java/org/example/leaderboard/LeaderboardDatabaseStub.java
@@ -1,11 +1,10 @@
 package org.example.leaderboard;
 
-import java.nio.file.attribute.AclEntry;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.example.Player;
 
-import javax.swing.text.StyledEditorKit;
 
 /**
  * Stub class for the database interface
@@ -15,17 +14,39 @@ public class LeaderboardDatabaseStub  {
 
     public LeaderboardDatabaseStub(){
         players = new ArrayList<>();
+        initializePlayers();
     }
 
-    // initialize Alice
-    Player alice = new Player("Alice");
-    alice.setCheckerWins(2);
-    alice.setCheckerLosses(5);
-    alice.setConnect4Wins(5);
-    alice.setConnect4losses(2);
-    alice.setTicTacToeWins(15);
-    alice.setTicTacToeLosses(12);
-    
+    public void initializePlayers(){
+    // add Alice    
+        Player alice = new Player("Alice");
+        alice.setCheckerWins(2);
+        alice.setCheckerLosses(5);
+        alice.setConnect4Wins(5);
+        alice.setConnect4Losses(2);
+        alice.setTictactoeWins(15);
+        alice.setTictactoeLosses(12);
+        players.add(alice);
+
+        // add Bob
+        Player bob = new Player("Bob");
+        bob.setCheckerWins(3);
+        bob.setCheckerLosses(4);
+        bob.setConnect4Wins(4);
+        bob.setConnect4Losses(3);
+        bob.setTictactoeWins(10);
+        bob.setTictactoeLosses(15);
+        players.add(bob);
+
+        // add Charlie
+        Player charlie = new Player("Charlie");
+        charlie.setCheckerWins(7);
+        charlie.setCheckerLosses(10);
+        charlie.setConnect4Wins(16);
+        charlie.setConnect4Losses(10);
+        charlie.setTictactoeWins(13);
+        charlie.setTictactoeLosses(7);
+    }
     
 
 

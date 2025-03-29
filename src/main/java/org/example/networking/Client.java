2576e3f9ac8b4c8c7026f873b6a8fa2bbfa2796d
30208515 <eloisa.castillo@ucalgary.ca>
2025-03-29 13:25:07 -0600

    Finished connect method and disconnect method

--- src/main/java/org/example/networking/Client.java
+++ src/main/java/org/example/networking/Client.java
@@ -1,9 +1,17 @@
 package org.example.networking;
 public class Client {
+    //direct access to these is restricted (they are private) for encapsulation
+    //instead we provide controlled access through getters and setters
     private String serverAddress;
-    boolean isConnected = false;
-    public boolean Connect(String address){
-        System.out.println("Attempting to connect to server at "+ address + "...");
+    private boolean isConnected = false;
+    private UserProfile userProfile;
+
+
+    public Client(){
+        this.isConnected = false;
+    }
+    public boolean connect(String address, int port){
+        System.out.println("Attempting to connect to server at "+ address +" on port " + port + "...");
         if(!isConnected){
             //sleep the program for 1 second to simulate a real connection attempt
             //Thread.sleep is in a try/catch block because although an interruption in the fake connection attempt is unlikely, it's good practice in case the thread running Thread.sleep is interrupted by another thread
@@ -12,11 +20,25 @@ public class Client {
             }catch(InterruptedException e){
                 System.out.println("Connection attempt was interrupted!")
             }
-            isConnected=true;
+            this.isConnected=true;
+            this.serverAddress = address;
             System.out.println("Connected to server successfully!");
+            return true;
+        }else{
+            System.out.println("Already connected!");
+            return false;
         }
     }
 
+    public void disconnect(){
+        System.out.println("Disconnecting from server...");
+        if(!isConnected){
+            System.out.println("No connection to disconnect.");
+        }else{
+            this.isConnected = false;
+            System.out.println("Disconnected successfully.");
+        }
+    }
 
 
 

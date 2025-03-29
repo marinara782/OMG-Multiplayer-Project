3a97952967855b2af97100216600232d70ceddc1
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-28 23:51:42 -0600

    Coming up with very basic structure for SkillBasedMatchmaking.java

--- src/main/java/org/example/matchmaking/SkillBasedMatchmaking.java
+++ src/main/java/org/example/matchmaking/SkillBasedMatchmaking.java
@@ -1,4 +1,9 @@
 package org.example.matchmaking;
 
 public class SkillBasedMatchmaking {
+
+    // Basic structure:
+    //if players skill level is on par with another players skill level
+    // they should play together
+    // otherwise, continue searching and prevent search from picking up that user again
 }

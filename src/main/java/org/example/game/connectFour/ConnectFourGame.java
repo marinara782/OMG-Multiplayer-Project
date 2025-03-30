8b72c7f409558b9d5576aca8581cf298d5cbac3a
30227411 <hamza.ata@ucalgary.ca>
2025-03-30 15:19:00 -0600

    worked on gameLogic
    created createBoard method
    desgining ConnectFourGame.java

--- src/main/java/org/example/game/connectFour/ConnectFourGame.java
+++ src/main/java/org/example/game/connectFour/ConnectFourGame.java
@@ -2,4 +2,26 @@ package org.example.game.connectFour;
 
 public class ConnectFourGame {
 
+    public int player;
+    public int rows;
+    public int columns;
+    public int goal;
+    public int[][] board;
+
+    public ConnectFourGame(int player, int rows, int columns, int goal) {
+        this.player = player;
+        this.rows = rows;
+        this.columns = columns;
+        this.goal = goal;
+    }
+
+    public int[][] getBoard() {
+        return this.board;
+    }
+
+    public int getPlayer() {
+        return this.player;
+    }
+
+
 }

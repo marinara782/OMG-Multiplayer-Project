bfd95fa8aae7d8352f53045e217c46bd77c7d50e
30177972 <vihandu.appuhamy@ucalgary.ca>
2025-03-31 13:07:41 -0600

    forgot_password added

--- src/main/java/org/example/authentication/Login.java
+++ src/main/java/org/example/authentication/Login.java
@@ -2,10 +2,31 @@ package org.example.authentication;
 
 import javafx.stage.Stage;
 
-public class Login {
+import java.io.FileNotFoundException;
+
+public class Login extends UserDatabaseStub{
     public Login(Stage stage) {
     }
 
     public void show() {
     }
+    public boolean forgot_password(String username, String email, String newPassword) throws FileNotFoundException{
+        //verify that the username exists
+        if(!verify_username(username)){
+            System.out.println("useername does not exist");
+            return false;
+        }
+        //verify that the email matchs the username
+        if(!verify_email(username, email)){
+            System.out.println(("Email does not match username"));
+            return false;
+        }
+        //get current password to change
+        String currentPassword = getCurrentPassword(username);
+
+        update_password(username, currentPassword, newPassword);
+        System.out.println("password changed sucessfully");
+        return true;
+    }
+
 }

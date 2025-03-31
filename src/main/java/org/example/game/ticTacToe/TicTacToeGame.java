bc61df2a48882f75a1fc8337220c798b8f9e919c
Abdu <abdurahmann.benissa@ucalgary.ca>
2025-03-31 14:11:25 -0600

    Currently working on the checkForWin() method. Just finished writing the code for possible diagonal wins. Now will be working on the columns possible wins.

--- src/main/java/org/example/game/ticTacToe/TicTacToeGame.java
+++ src/main/java/org/example/game/ticTacToe/TicTacToeGame.java
@@ -11,7 +11,7 @@ public class TicTacToeGame {
         this.sizeOfTheBoard = 3;
         this.player = 'O';
         this.board = new char[3][3];
-        //We also have to initialize the cells of the board, they need to be empty at first
+        //We also have to initialize the cells of the board, they need to be empty at first, whenever a game starts
 
        BoardInitialization();
     }
@@ -20,15 +20,29 @@ public class TicTacToeGame {
     {
         board = new char[3][3];
         for(short i = 0; 3>i;i++) {
-            for (short j = 0; 3 > i; i++) {
+            for (short j = 0; 3 > j; j++) {
                 board[i][j] = '/';
             }
         }
     }
 
 
-    public boolean checkForWin() {
-        return false;
+    public boolean checkForWin()
+    {
+        //There are 3 possibilities for a win. We need to check diagonals, columns and rows.
+
+        //Diagonal Part:
+        //The board is 3 by 3, and we could only have 2 diagonals.
+        //Diagonal 1:
+        if((board[0][0] == player) && (board[1][1] == player) && (board[2][2] == player))
+            return true; // true meaning, this is a win, if condition is fulfilled
+
+        //Diagonal 2:
+        //So we could have a diagonal win from the left side (Diagonal Part 1) or we could have a diagonal win form the right side, which is the following:
+
+        if((board[2][2] == player) && (board[1][1] == player) && (board[2][0] == player))
+            return true; // true meaning, this is a win, if condition is fulfilled
+
     }
 
     public boolean isBoardFull() {

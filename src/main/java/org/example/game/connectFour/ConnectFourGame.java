916c2017d6cf549ced431363cf3a1efdf7b54b34
30241942 <shazil.khan@ucalgary.ca>
2025-04-01 13:52:37 -0600

    UseCaseDiagram for Matchmaking_usecasediagram.png

--- src/main/java/org/example/game/connectFour/ConnectFourGame.java
+++ src/main/java/org/example/game/connectFour/ConnectFourGame.java
@@ -7,14 +7,17 @@ public class ConnectFourGame {
     public int columns;
     public int goal;
     public int[][] board;
+    private boolean vsComputer;
 
-    public ConnectFourGame(int player, int rows, int columns, int goal) {
+    public ConnectFourGame(int player, int rows, int columns, int goal, boolean vsComputer) {
         this.player = player;
         this.rows = rows;
         this.columns = columns;
         this.goal = goal;
+        this.vsComputer = vsComputer;
         this.board = ConnectFourBoard.createBoard(rows, columns);
     }
+
 
     public int[][] getBoard() {
         return this.board;
@@ -37,110 +40,116 @@ public class ConnectFourGame {
     }
 
     public boolean makeMove(int row, int column) {
-        if(this.board[row][column] == ConnectFourBoard.Empty) {
+        if (this.board[row][column] == ConnectFourBoard.Empty) {
             this.board[row][column] = player;
             return true;
         }
         return false;
     }
 
-    public void switchTurn(){
-        if(player == ConnectFourBoard.Red){
+    public void switchTurn() {
+        if (player == ConnectFourBoard.Red) {
             player = ConnectFourBoard.Blue;
-        }
-        else if(player == ConnectFourBoard.Blue){
+        } else if (player == ConnectFourBoard.Blue) {
             player = ConnectFourBoard.Red;
         }
     }
+
     public boolean checkWinnerHorizontal() {
 
         int counter = 0;
-        for(int row = 0; row < rows; row++){
-            for(int column = 0; column < columns; column++){
-                if(this.board[row][column] == player){
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (this.board[row][column] == player) {
                     counter++;
-                    if(counter >= goal){
+                    if (counter >= goal) {
                         return true;
                     }
-                }else {
+                } else {
                     counter = 0;
                 }
             }
         }
         return false;
     }
+
     public boolean checkWinnerVertical() {
         int counter = 0;
-        for(int column = 0; column < columns; column++){
-            for(int row = 0; row < rows; row++){
-                if(this.board[row][column] == player){
+        for (int column = 0; column < columns; column++) {
+            for (int row = 0; row < rows; row++) {
+                if (this.board[row][column] == player) {
                     counter++;
-                    if(counter >= goal){
+                    if (counter >= goal) {
                         return true;
                     }
-                }else {
+                } else {
                     counter = 0;
                 }
             }
         }
         return false;
     }
+
     public boolean checkWinnerDiagonal() {
         int counter = 0;
-        for(int row = 0; row <= rows - goal; row++){
-            for (int col = 0; col <= columns - goal; col++){
+        for (int row = 0; row <= rows - goal; row++) {
+            for (int col = 0; col <= columns - goal; col++) {
                 counter = 0;
-                for(int i = 0 ; i < goal; i++){
-                    if(this.board[row + i][col + i] == player){
-                        counter++;
-                    }else {
-                        break;
-                    }
-                }
-                if (counter == goal){
-                    return true;
-                }
-            }
-        }
-
-        for (int row = goal -1 ; row < rows; row++){
-            for (int col = 0 ;  col <= columns - goal; col++){
-                counter = 0;
-                for(int i = 0 ; i < goal; i++){
-                    if(this.board[row - i][col + i] == player){
+                for (int i = 0; i < goal; i++) {
+                    if (this.board[row + i][col + i] == player) {
                         counter++;
                     } else {
                         break;
                     }
                 }
-                if (counter == goal){
+                if (counter == goal) {
+                    return true;
+                }
+            }
+        }
+
+        for (int row = goal - 1; row < rows; row++) {
+            for (int col = 0; col <= columns - goal; col++) {
+                counter = 0;
+                for (int i = 0; i < goal; i++) {
+                    if (this.board[row - i][col + i] == player) {
+                        counter++;
+                    } else {
+                        break;
+                    }
+                }
+                if (counter == goal) {
                     return true;
                 }
             }
         }
         return false;
     }
+
     public boolean checkDraw() {
 
-        for (int row = 0; row < rows; row++){
-            for (int column = 0; column < columns; column++){
-                if(this.board[row][column] == ConnectFourBoard.Empty){
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (this.board[row][column] == ConnectFourBoard.Empty) {
                     return false;
                 }
 
             }
         }
 
-        if(checkWinnerHorizontal() == false && checkWinnerVertical() == false && checkWinnerDiagonal() == false){
+        if (!checkWinnerHorizontal() && !checkWinnerVertical() && !checkWinnerDiagonal()) {
             return false;
         }
-
-        return true;
-    }
 
 // because we're using GUI for now we can keep this method empty
 //    public void printBoard(){
 //
 //    }
+    return checkDraw();
+    }
 
+    public boolean isVsComputer() {
+        return vsComputer;
+    }
 }
+

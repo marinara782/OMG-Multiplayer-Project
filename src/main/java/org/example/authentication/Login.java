00d32ce0ee2ab575db1048b064f8692893f40850
30177972 <vihandu.appuhamy@ucalgary.ca>
2025-04-02 13:06:47 -0600

    change_password() added and UserDatabaseStub updated

--- src/main/java/org/example/authentication/Login.java
+++ src/main/java/org/example/authentication/Login.java
@@ -64,21 +64,50 @@ public class Login extends UserDatabaseStub {
 
     public static boolean createAccount(String new_username, String password, String email, String phone) {
 
-            System.out.println("\nAttempting to create account for: " + new_username);
+        System.out.println("\nAttempting to create account for: " + new_username);
 
-            UserDatabaseStub databaseStub = new UserDatabaseStub();
+        UserDatabaseStub databaseStub = new UserDatabaseStub();
         // authenticate users
         try {
-            if(databaseStub.Authenticate_user(new_username, password, email, phone)){
+            if (databaseStub.Authenticate_user(new_username, password, email, phone)) {
                 System.out.println("User already exists.");
-                return false;}
+                return false;
+            }
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException("User database file not found.");
         }
         System.out.println("✅ Account successfully created for: " + new_username);
         return true;
+    }
+
+    public boolean change_password(String username, String oldPassword, String newPassword, String confirmPassword) throws FileNotFoundException{
+        //verify old password
+        if(!verify_password(username, oldPassword)){
+            System.out.println("Password is incorrect");
+            return false;
         }
 
+        //newpassowrd has length >=8
+        boolean isLongEnough = newPassword.length() >= 8;
+        //new password has at least one letter
+        boolean hasLetter = newPassword.matches(".*[a-zA-Z].*");
+        //new password has at least 1 digit
+        boolean hasNumber= newPassword.matches(".*\\d.*");
+        if(!(isLongEnough && hasLetter && hasNumber)){
+            System.out.println("newPassword does not match requirements: it must be at east 8 characters long and contain at least 1 letter and 1 number ");
+        }
+
+        //verify that new password matches confirmation input
+        if(!newPassword.equals(confirmPassword)){
+            System.out.println("New password and confirmation do not match");
+            return false;
+        }
+        //change password
+        update_password(username, oldPassword, newPassword);
+        System.out.println("Password changed successfully");
+        return true;
     }
+
+}
 

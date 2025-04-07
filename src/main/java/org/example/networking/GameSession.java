c7b41f1752e58a261e16db2050ab51fa2e58d72d
30241942 <shazil.khan@ucalgary.ca>
2025-04-07 16:44:47 -0600

    Manually updating the networking class files because merge didnt work out

--- src/main/java/org/example/networking/GameSession.java
+++ src/main/java/org/example/networking/GameSession.java
@@ -33,6 +33,37 @@ public class GameSession {
     }
 
     public void waitForOpponentMove(Object o) {
+        System.out.print("Waiting for opponent's move...");
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Connection interrupted!");
+        }
+    }
 
+    public String processMove(Client player, String moveData) {
+        if (players.contains(player)){
+            System.out.println("Processing move: " + moveData);
+            return ("Move processed: " +moveData);
+        }else{
+            return "Player is not part of this session!";
+        }
+
+    }
+
+    public void sendChat(String message){
+        System.out.println("Sending chat: " + message);
+    }
+
+    public void completeSession(){
+        System.out.println("Complete game session...");
+    }
+
+    public int getSessionid()
+    {
+        return sessionid;
     }
 }

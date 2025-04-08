6bab3dd7afa17c438fd55cebb9b1e4eea5d053fa
30075408 <syed.zaidi3@ucalgary.ca>
2025-04-07 23:50:50 -0600

    Created enum for StatusOptions and integrated with User class, added method to change the status of a User

--- src/main/java/org/example/authentication/User.java
+++ src/main/java/org/example/authentication/User.java
@@ -5,12 +5,14 @@ public class User {
     String password;
     String email;
     String phone;
+    StatusOptions current_status;
 
     public User(String username, String password, String email, String phone) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.phone = phone;
+        this.current_status = StatusOptions.OFFLINE;
     }
 
     public String getUsername() {
@@ -36,5 +38,11 @@ public class User {
     }
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+    public StatusOptions getCurrentStatus() {
+        return current_status;
+    }
+    public void setCurrentStatus(StatusOptions current_status) {
+        this.current_status = current_status;
     }
 }

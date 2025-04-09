23d65ec863d086ca0fb86309d2bc71a3d4874b84
30237507 <marena.matavao@ucalgary.ca>
2025-04-09 13:18:13 -0600

    Saving my changes before pulling from iteration3 branch

--- src/test/java/org/example/PlayerTest.java
+++ src/test/java/org/example/PlayerTest.java
@@ -1,4 +1,155 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+    private Player player
+    @Test
+    void updateTotalWins() {
+    }
+
+    @Test
+    void updateTotalLosses() {
+    }
+
+    @Test
+    void updateTotalDraws() {
+    }
+
+    @Test
+    void updateWinPercentage() {
+    }
+
+    @Test
+    void setCheckerWins() {
+    }
+
+    @Test
+    void setCheckerLosses() {
+    }
+
+    @Test
+    void setTictactoeWins() {
+    }
+
+    @Test
+    void setTictactoeLosses() {
+    }
+
+    @Test
+    void setTictactoeDraws() {
+    }
+
+    @Test
+    void setConnect4Wins() {
+    }
+
+    @Test
+    void setConnect4Losses() {
+    }
+
+    @Test
+    void setConnect4Draws() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getCheckerWins() {
+    }
+
+    @Test
+    void getCheckerLosses() {
+    }
+
+    @Test
+    void getCheckersWinPercentage() {
+    }
+
+    @Test
+    void getTictactoeWins() {
+    }
+
+    @Test
+    void getTictactoeLosses() {
+    }
+
+    @Test
+    void getTictactoeDraws() {
+    }
+
+    @Test
+    void getTicTacToeWinPercentage() {
+    }
+
+    @Test
+    void getConnect4Wins() {
+    }
+
+    @Test
+    void getConnect4Losses() {
+    }
+
+    @Test
+    void getConnect4Draws() {
+    }
+
+    @Test
+    void getConnect4WinPercentage() {
+    }
+
+    @Test
+    void getTotalWins() {
+    }
+
+    @Test
+    void getTotalLosses() {
+    }
+
+    @Test
+    void getWinPercentage() {
+    }
+
+    @Test
+    void getTotalDraws() {
+    }
+
+    @Test
+    void updateCheckerWins() {
+    }
+
+    @Test
+    void updateCheckerLosses() {
+    }
+
+    @Test
+    void updateTicTacToeWins() {
+    }
+
+    @Test
+    void updateTictactoeLosses() {
+    }
+
+    @Test
+    void updateTictactoeDraws() {
+    }
+
+    @Test
+    void updateConnect4Wins() {
+    }
+
+    @Test
+    void updateConnect4Losses() {
+    }
+
+    @Test
+    void updateConnect4Draws() {
+    }
 }
